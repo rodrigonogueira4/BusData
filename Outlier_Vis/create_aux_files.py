33643a7ba432bd774a1108dcc67f34085e00171f
Rodrigo <rodrigonogueira4@gmail.com>
2015-06-27 11:28:53 -0400

    mods line bar chart

--- Outlier_Vis/create_aux_files.py
+++ Outlier_Vis/create_aux_files.py
@@ -5,10 +5,11 @@ import dateutil.parser
 in_dir = "outliers/"
 out_file_per_day = "number_of_outliers_per_day.csv"
 out_file_per_day_hour_line = "number_of_outliers_per_day_hour_line.csv"
+out_file_per_day_line = "number_of_outliers_per_day_line.csv"
 files = [os.path.join(dp, f) for dp, _, filenames in os.walk(in_dir) for f in filenames]
 
 dic_num_per_day = {}
-dic_num_per_line = {}
+dic_num_per_day_line = {}
 dic_num_per_day_line_hour = {}
 for in_file in files:
     day_month = in_file.replace(in_dir,"").replace(".csv", "")
@@ -17,14 +18,22 @@ for in_file in files:
     with open(in_file) as f:
         rows = csv.DictReader(f, delimiter=',')
         for row in rows:
-            line = row["Linha"]
+            line = row["Linha"] 
+            busid = row["Onibus"]
             timestamp = dateutil.parser.parse(row["TimeStamp_0"])
             hour = timestamp.hour
             year = timestamp.year
+
             key = str(year) + "," + str(month) + "," + str(day) + "," + str(hour) + "," + str(line)
             if key not in dic_num_per_day_line_hour:
                 dic_num_per_day_line_hour[key] = 0
             dic_num_per_day_line_hour[key] += 1
+
+            key2 = str(year) + "," + str(month) + "," + str(day) + "," + str(line)
+            if key2 not in dic_num_per_day_line:
+                dic_num_per_day_line[key2] = {}
+            dic_num_per_day_line[key2][busid] = 0
+
             num_outliers += 1
         dic_num_per_day[100*int(month)+int(day)] = str(num_outliers) + "," + month + "/" + day +"\n"
 
@@ -41,4 +50,11 @@ fout = open(out_file_per_day_hour_line, "wb")
 fout.write("\"Year\",\"Month\",\"Day\",\"Hour\",\"Line\",\"NumberOfOutliers\"\n")
 for key, value in dic_num_per_day_line_hour.items():
     fout.write(str(key) + "," + str(value)+"\n")
+fout.close()
+
+#write the number of outliers per day and line
+fout = open(out_file_per_day_line, "wb")
+fout.write("\"Year\",\"Month\",\"Day\",\"Line\",\"NumberOfOutliers\"\n")
+for key, value in dic_num_per_day_line.items():
+    fout.write(str(key) + "," + str(len(value))+"\n")
 fout.close()
