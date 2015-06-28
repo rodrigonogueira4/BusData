640a370b09cc6f4147c641b49e26cda85129dd7e
Rodrigo <rodrigonogueira4@gmail.com>
2015-06-27 22:49:08 -0400

    minor changes

--- Outlier_Vis/create_aux_files.py
+++ Outlier_Vis/create_aux_files.py
@@ -5,12 +5,15 @@ import dateutil.parser
 in_dir = "outliers/"
 out_file_per_day = "number_of_outliers_per_day.csv"
 out_file_per_day_hour_line = "number_of_outliers_per_day_hour_line.csv"
+out_file_per_day_hour_line_bus = "number_of_outliers_per_day_hour_line_bus.csv"
 out_file_per_day_line = "number_of_outliers_per_day_line.csv"
 files = [os.path.join(dp, f) for dp, _, filenames in os.walk(in_dir) for f in filenames]
 
 dic_num_per_day = {}
 dic_num_per_day_line = {}
 dic_num_per_day_line_hour = {}
+dic_num_per_day_hour_line_bus = {}
+
 for in_file in files:
     day_month = in_file.replace(in_dir,"").replace(".csv", "")
     day, month = day_month.split("_")
@@ -23,6 +26,11 @@ for in_file in files:
             timestamp = dateutil.parser.parse(row["TimeStamp_0"])
             hour = timestamp.hour
             year = timestamp.year
+
+            key = str(year) + "," + str(month) + "," + str(day) + "," + str(hour) + "," + str(line) + "," + str(busid)
+            if key not in dic_num_per_day_hour_line_bus:
+                dic_num_per_day_hour_line_bus[key] = 0
+            dic_num_per_day_hour_line_bus[key] += 1
 
             key = str(year) + "," + str(month) + "," + str(day) + "," + str(hour) + "," + str(line)
             if key not in dic_num_per_day_line_hour:
@@ -58,3 +66,12 @@ fout.write("\"Year\",\"Month\",\"Day\",\"Line\",\"NumberOfOutliers\"\n")
 for key, value in dic_num_per_day_line.items():
     fout.write(str(key) + "," + str(len(value))+"\n")
 fout.close()
+
+
+#write the number of outliers per day, hour line and bus
+fout = open(out_file_per_day_hour_line_bus, "wb")
+fout.write("\"Year\",\"Month\",\"Day\",\"Hour\",\"Line\",\"BusId\",\"NumberOfOutliers\"\n")
+for key, value in dic_num_per_day_hour_line_bus.items():
+    fout.write(str(key) + "," + str(value)+"\n")
+fout.close()
+
